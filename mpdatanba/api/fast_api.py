e99274d9229479ef7a62dfca2fd5001a818c21c0
Raman Sheshka <raman.sheshka@gmail.com>
2024-11-05 23:03:36 +0100

    fastapi predict tampon function

--- mpdatanba/api/fast_api.py
+++ mpdatanba/api/fast_api.py
@@ -16,7 +16,38 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
+@app.get("/predict/")
+def predict():
+    data = pd.DataFrame({
+        'PTS': [110],
+        'FGM': [40],
+        'FGA': [90],
+        'FG%': [0.44],
+        '3PM': [10],
+        '3PA': [30],
+        '3P%': [0.33],
+        'FTM': [20],
+        'FTA': [30],
+        'FT%': [0.67],
+        'OREB': [10],
+        'DREB': [30],
+        'REB': [40],
+        'AST': [20],
+        'TOV': [15],
+        'STL': [10],
+        'BLK': [5],
+        'PF': [20],
+        'FP': [100],
+        'DD2': [5],
+        'TD3': [1]
+    })
+
+    model = app.state.model
+    #prediction = model.predict(data)
+    #return {'prediction': prediction.tolist()}
+    return dict(prediction=np.array([1, 0]))
+
 @app.get("/")
 def root():
-    return {'greeting': 'Hello Word!',
+    return {'message': 'Hello Word!',
            }
--- mpdatanba/ml_logic/ml_workflow.py
+++ mpdatanba/ml_logic/ml_workflow.py
@@ -2,6 +2,34 @@ import glob
 import os
 import time
 
+def preprocess_data():
+    # Preprocess the data
+    print("Preprocessing data...")
+    time.sleep(2)
+    print("Data preprocessed!")
+    return None
+
+def train_model():
+    # Train the model
+    print("Training model...")
+    time.sleep(2)
+    print("Model trained!")
+    return None
+
+def evaluate_model():
+    # Evaluate the model
+    print("Evaluating model...")
+    time.sleep(2)
+    print("Model evaluated!")
+    return None
+
+def predict():
+    # Make predictions
+    print("Making predictions...")
+    time.sleep(2)
+    print("Predictions made!")
+    return None
+
 def load_model():
     # Load the model
     print("Loading model...")
